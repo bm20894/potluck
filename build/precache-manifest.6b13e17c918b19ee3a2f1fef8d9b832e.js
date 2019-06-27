4857b0394b124f3d541cc97347873b954c63a53a
bm20894 <milesboz@me.com>
2019-06-27 15:15:21 -0500

    package.json and build

---
+++ build/precache-manifest.6b13e17c918b19ee3a2f1fef8d9b832e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6abc2555b73a7cb86dd64c5551916eed",
+    "url": "/potluck/index.html"
+  },
+  {
+    "revision": "3f61c2aa24cd682476bc",
+    "url": "/potluck/static/css/main.3f122b58.chunk.css"
+  },
+  {
+    "revision": "4c72b79bf862f144f150",
+    "url": "/potluck/static/js/2.c56677ec.chunk.js"
+  },
+  {
+    "revision": "3f61c2aa24cd682476bc",
+    "url": "/potluck/static/js/main.dcd335d9.chunk.js"
+  },
+  {
+    "revision": "4b27f419fb7eb6db973b",
+    "url": "/potluck/static/js/runtime~main.1ea487c3.js"
+  },
+  {
+    "revision": "5e02af51a044f8d6de136e2c7e8d6c9a",
+    "url": "/potluck/static/media/spinner.5e02af51.gif"
+  }
+]);
